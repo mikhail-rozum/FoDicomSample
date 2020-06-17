8e32f242b7eb08995c9935ef63137098a2461384
Mikhail Rozum <RozumMV@sibedge.com>
2020-06-17 19:35:30 +0700

    Initial commit

---
+++ FoDicomSample/Program.cs
@@ -0,0 +1,23 @@
+﻿namespace FoDicomSample
+{
+    using System;
+
+    using Dicom;
+    using Dicom.Imaging.Codec;
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            TranscoderManager.SetImplementation(new Efferent.Native.Codec.NativeTranscoderManager());
+
+            var path = @"1.3.6.1.4.1.41327.1412220253660.4803989431405291768.dcm";
+
+            var dcm = DicomFile.Open(path);
+            var convertedFile = dcm.Clone(DicomTransferSyntax.DeflatedExplicitVRLittleEndian);
+            var pixelData = convertedFile.Dataset.GetValues<byte>(DicomTag.PixelData);
+
+            Console.ReadKey();
+        }
+    }
+}
